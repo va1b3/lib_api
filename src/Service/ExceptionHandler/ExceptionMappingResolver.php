a0e15d29a6b47119b39cd3ac681a9407042b1854
va1b3 <inc.valbe@gmail.com>
2022-10-11 02:31:02 +1000

    Exception handler added

---
+++ src/Service/ExceptionHandler/ExceptionMappingResolver.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Service\ExceptionHandler;
+
+use InvalidArgumentException;
+
+class ExceptionMappingResolver
+{
+    /**
+     * @var ExceptionMapping[]
+     */
+    private array $mappings;
+
+    public function __construct(array $mappings)
+    {
+        foreach ($mappings as $class => $mapping) {
+            if (empty($mapping['code'])) {
+                throw new InvalidArgumentException(
+                    'code is required for class' . $class);
+            }
+
+            $this->addMapping(
+                $class,
+                $mapping['code'],
+                $mapping['hidden'] ?? true,
+                $mapping['loggable'] ?? false
+            );
+        }
+    }
+
+    private function addMapping(
+        string $class,
+        int $code,
+        bool $hidden,
+        bool $loggable
+    ): void {
+        $this->mappings[$class] = new ExceptionMapping($code, $hidden,
+            $loggable);
+    }
+
+    public function resolve(string $throwableClass): ?ExceptionMapping
+    {
+        $foundMapping = null;
+        foreach ($this->mappings as $class => $mapping) {
+            if ($throwableClass === $class or is_subclass_of($throwableClass,
+                    $class)
+            ) {
+                $foundMapping = $mapping;
+                break;
+            }
+        }
+
+        return $foundMapping;
+    }
+}
